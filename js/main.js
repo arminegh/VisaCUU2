052598420d8739b69ffda0203e4f60992cb5b4b8
arminegh <38959509+arminegh@users.noreply.github.com>
2025-06-26 14:05:29 -0600

    curp verificada e inicio de menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 
 // estos serían los expedientes que existen según su CURP 
 const CURPexpedientes = ["GAHA771209MCHRRR08", "RETU870403HSOUYE76", "POLJ951108MDFUHS67"]
+// se cargarían de una base de información ya existente
+const exp01 = ["GAHA771209MCHRRR08", true, "BRIGHT VIEW", "+526141423794"]
+const exp02 = ["RETU870403HSOUYE76", false, "", ""]
+const exp03 = ["POLJ951108MDFUHS67", true, "LAWN SCAPES", "+13457687123"]
+
+
 let existeCURP = false
 
 function buscarExp (exp) {
@@ -15,8 +21,8 @@ buscarExp (CURP)
 console.log (existeCURP)
 
 
-let menu = parseInt(prompt("1-Revisar si estoy pedido \n 2-Agregar experiencia \n 3-Revisar mi teléfono de contacto\n 4-Modificar mi teléfono de contacto \n 5-Salir"))
-while(existeCURP && menu !=5 ) {
+let menu = parseInt(prompt("1-Revisar si estoy pedido \n 2-Revisar mi teléfono de contacto\n 3-Modificar mi teléfono de contacto \n 4-Salir"))
+while(existeCURP && menu !=4 ) {
     switch(menu) {
         case 1:
             
@@ -27,13 +33,10 @@ while(existeCURP && menu !=5 ) {
         case 3:
             
             break
-        case 4:
-            
-            break
         default:
             alert("Opcion incorrecta")
     }
-    menu = parseInt(prompt("1-Revisar si estoy pedido \n 2-Agregar experiencia \n 3-Revisar mi teléfono de contacto\n 4-Modificar mi teléfono de contacto \n 5-Salir"))
+    menu = parseInt(prompt("1-Revisar si estoy pedido \n 2-Revisar mi teléfono de contacto\n 3-Modificar mi teléfono de contacto \n 4-Salir"))
 }
 
 
