a77bab5134e87156e6132a3ad35be210c6055aed
arminegh <38959509+arminegh@users.noreply.github.com>
2025-06-26 17:31:40 -0600

    correccion de validacion de curp para el while

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const exp01 = ["GAHA771209MCHRRR08", true, "BRIGHT VIEW", "+526141423794"]
 const exp02 = ["RETU870403HSOUYE76", false, "", ""]
 const exp03 = ["POLJ951108MDFUHS67", true, "LAWN SCAPES", "+13457687123"]
 let existeCURP = false
+let menu = 4
 //
 
 function buscarExp (exp) {
@@ -38,9 +39,17 @@ let CURP = prompt("Ingresa tu CURP").toUpperCase();
 buscarExp (CURP)
 
 
+if (!existeCURP == true) {
+    alert ("No existe expediente para esa CURP en VisaCUU")
+    menu = 4}
+else {
+    menu = parseInt(prompt(" 1-Revisar si estoy pedido \n 2-Revisar mi teléfono de contacto\n 3-Modificar mi teléfono de contacto \n 4-Salir"))
+}
 
-let menu = parseInt(prompt(" 1-Revisar si estoy pedido \n 2-Revisar mi teléfono de contacto\n 3-Modificar mi teléfono de contacto \n 4-Salir"))
-while(existeCURP && menu !=4 ) {
+
+
+while ( (existeCURP) && (menu !=4) ) {
+    
     switch(menu) {
         case 1:
             tienePeticion (CURP)
@@ -61,5 +70,3 @@ while(existeCURP && menu !=4 ) {
 
 
 
-if (!existeCURP == true) {
-    alert ("No existe expediente para esa CURP en VisaCUU")}
