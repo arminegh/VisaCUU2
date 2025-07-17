00afb5b7db121627a56ca6836f9bd501cbee6233
arminegh <38959509+arminegh@users.noreply.github.com>
2025-07-17 12:47:14 -0600

    primera transicion a uso del DOM

--- js/main.js
+++ js/main.js
@@ -2,41 +2,106 @@
 // estos serían los expedientes que existen según su CURP 
 const CURPexpedientes = ["GAHA771209MCHRRR08", "RETU870403HSOUYE76", "POLJ951108MDFUHS67"]
 // se cargarían de una base de información ya existente
-const exp01 = ["GAHA771209MCHRRR08", true, "BRIGHT VIEW", "+526141423794"]
-const exp02 = ["RETU870403HSOUYE76", false, "", ""]
-const exp03 = ["POLJ951108MDFUHS67", true, "LAWN SCAPES", "+13457687123"]
+
+
+const expedientes= [
+    {
+        curp: "GAHA771209MCHRRR08",
+        peticion: true, 
+        empresa: "BRIGHT VIEW",
+        telefono: "+526141423794" 
+    },
+    {
+        curp: "RETU870403HSOUYE76", 
+        peticion: false, 
+        empresa: "",
+        telefono: "+526394732334"
+    },
+    {
+        curp: "ABC123", 
+        peticion: false, 
+        empresa: "",
+        telefono: "+1234876546351"
+    },
+    {
+        curp: "XYZ098", 
+        peticion: false, 
+        empresa: "",
+        telefono: "+1234876546351"
+    },
+    {
+        curp: "123", 
+        peticion: true, 
+        empresa: "ELEMENTARY",
+        telefono: "+1234876546351"
+    },
+]
+
+
 let existeCURP = false
 let menu = 4
-//
 
-function buscarExp (exp) {
-    if (CURPexpedientes.includes(exp)){
-        existeCURP = true;
-    }
-   
+
+let CURP = document.getElementById("CURP");
+let comunicarOpciones = document.getElementById("comunicarOpciones");
+let peticion = document.getElementById("peticion");
+let telefono = document.getElementById("telefono");
+let evento = document.getElementById ("CURP");
+
+
+
+
+evento.addEventListener ("change", CURPingresado) 
+
+function CURPingresado () {
+    CURP.value = (CURP.value).toUpperCase();
 }
 
-function tienePeticion (exp) {
-    if (exp01[1] == true) {
-        console.log (exp01[1])
-        alert("si estas pedido por la compañía " + exp01[2])
+function buscarExp (exp) {
+    const usuarioActual = expedientes.find(expediente => expediente.curp === exp);
+    return usuarioActual;
+
+
+   
+}
+//función para guardar en local storage el expediente
+function descargarInfo (exp) {
+    const usuarioActual = expedientes.find(expediente => expediente.curp === exp);
+    localStorage.setItem("curp", usuarioActual.curp)
+    localStorage.setItem("peticion", usuarioActual.peticion)
+    localStorage.setItem("empresa",usuarioActual.empresa)
+    localStorage.setItem("telefono",usuarioActual.telefono)
+}
+
+function mostrarPeticion (exp) {
+    const usuarioActual = expedientes.find(expediente => expediente.curp === exp);
+
+    if (usuarioActual.peticion == true) {
+        
+        alert("si estas pedido por la compañía " + usuarioActual.empresa)
     }
     else {
         alert ("No estas pedido")
     }
 }
 
-function mostrarTelefono (exp) {
-    alert("Tu teléfono para contacto registrado es el " + exp01[3])
+function mostrarTelefono () {
+    alert("Tu teléfono para contacto registrado es el " + localStorage.getItem("telefono"))
+
 }
+
+
 
 function cambiarTelefono (exp) {
     exp01[3] = exp
 }
 
 
-let CURP = prompt("Ingresa tu CURP").toUpperCase();
-buscarExp (CURP)
+
+
+//ANTES DE DOM
+/*let CURP = prompt("Ingresa tu CURP").toUpperCase();
+//buscarExp (CURP)
 
 
 if (!existeCURP == true) {
@@ -68,5 +133,24 @@ while ( (existeCURP) && (menu !=4) ) {
     menu = parseInt(prompt(" 1-Revisar si estoy pedido \n 2-Revisar mi teléfono de contacto\n 3-Modificar mi teléfono de contacto \n 4-Salir"))
 }
 
+*/
 
+
+
+buscar.onclick = () => {
+    //console.log (CURP.value);   
+    //console.log (existeCURP);  
+
+    
+    if ( buscarExp (CURP.value) ){
+        comunicarOpciones.innerHTML = "Información principal"
+        descargarInfo(CURP.value);
+        mostrarPeticion(CURP.value);
+        mostrarTelefono();
+
+    }else {
+        comunicarOpciones.innerHTML = "NO EXISTE EXPEDIENTE CON ESA CURP"
+
+    }
+}
 
